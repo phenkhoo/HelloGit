8c41b74dfa24b4b259982c8e7fe31fc0e43d089d
Stephen Khosasih <phenkhoo@gmail.com>
2018-01-16 18:07:45 +0700

    Second Commit!

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -1,16 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+import java.util.Scanner;
 /**
  *
  * @author User
  */
 public class HelloGit {
     public static void main(String []args){
-        System.out.println("Hello Git");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input your name: ");
+        String name = input.nextLine();
+        System.out.println("Hello" + name);
     }
     
 }
